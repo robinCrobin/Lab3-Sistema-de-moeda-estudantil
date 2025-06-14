ef6fba4330cb31a0247e07d4e3cc650fef4258fb
MTaveiras <marcosctaveira@gmail.com>
2025-06-14 14:17:04 -0300

    Commit final com gitignore e dependencia da senha do sendgrid

--- code/src/main/java/com/example/Lab3/Lab3Application.java
+++ code/src/main/java/com/example/Lab3/Lab3Application.java
@@ -1,18 +1,19 @@
 package com.example.Lab3;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-   cd code
- * mvn clean package
-   mvn spring-boot:run
- */
 @SpringBootApplication
 public class Lab3Application {
 
     public static void main(String[] args) {
+        // Carrega variáveis do .env
+        Dotenv dotenv = Dotenv.configure().load();
+        
+        // Configura a variável de ambiente para o Spring
+        System.setProperty("SENDGRID_API_KEY", dotenv.get("SENDGRID_API_KEY"));
+        
         SpringApplication.run(Lab3Application.class, args);
     }
-
 }
